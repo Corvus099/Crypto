6fbe6fe9013e4420f7f0922c96628ec867fa6fd5
Corvus099 <100049439+Corvus099@users.noreply.github.com>
2022-03-22 09:49:50 -0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api.exchange.coinbase.com/products/BTC-USD/candles?granularity=900&start=2013-07-05&end=2013-07-06"
-
-headers = {"Accept": "application/json"}
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
